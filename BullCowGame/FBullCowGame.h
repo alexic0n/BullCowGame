19ae7d0fc463d9c0c1db1592280a5b66aaf70ce6
Alex Bruce <alexbruce225@gmail.com>
2019-01-07 19:16:04 +0000

    A simple CMD game.
    Created whilst learning C++ w/ a Udemy course.

---
+++ BullCowGame/FBullCowGame.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <string>
+
+// to make syntax Unreal friendly
+using FString = std::string;
+using int32 = int;
+
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+enum class EGuessStatus
+{
+	Invalid_Status,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
+};
+
+
+class FBullCowGame
+{
+public:
+	FBullCowGame(); // constructor
+
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+	bool IsGameWon() const;
+	EGuessStatus CheckGuessValidity(FString) const;
+
+	void Reset();
+	FBullCowCount SubmitValidGuess(FString);
+
+
+	// ^^ Please try and ignore this and focus on the interface above ^^
+private:
+	// see constructor for initialisation
+	int32 MyCurrentTry;
+	FString MyHiddenWord;
+	bool bGameIsWon;
+
+	bool IsIsogram(FString) const;
+	bool IsLowercase(FString) const;
+};
